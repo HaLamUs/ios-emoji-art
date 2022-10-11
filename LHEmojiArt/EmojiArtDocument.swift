ae031b88738b1a3356be96116899952fd8b6e2c4
HaLamUs <itzlamz9x@gmail.com>
2022-10-12 00:06:02 +0700

    feat: Change background image

--- LHEmojiArt/EmojiArtDocument.swift
+++ LHEmojiArt/EmojiArtDocument.swift
@@ -11,22 +11,63 @@ import SwiftUI
 
 class EmojiArtDocument: ObservableObject {
     // hold a Model
-    @Published private(set) var emojiArt: EmojiArtModel
+    @Published private(set) var emojiArt: EmojiArtModel {
+        didSet {
+            if emojiArt.background != oldValue.background {
+                fetchBackgroundImageDataIfNeed()
+            }
+        }
+    }
     
     init() {
         emojiArt = EmojiArtModel()
-        emojiArt.addEmoji("Lam", at: (-200, -100), size: 80)
-        emojiArt.addEmoji("Ha", at: (50, 100), size: 40)
+        emojiArt.addEmoji("🛻", at: (-200, -100), size: 80)
+        emojiArt.addEmoji("🏎", at: (50, 100), size: 40)
     }
     
     // dont necessary but nice to have
     var emojis: [EmojiArtModel.Emoji] { emojiArt.emojis }
     var background: EmojiArtModel.Background { emojiArt.background }
     
+    @Published var backgroundImage: UIImage?
+    @Published var backgroundImageFetchStatus = BackgroundImageFetchStatus.idle
+    
+    enum BackgroundImageFetchStatus {
+        case idle
+        case fetching
+        
+    }
+    
+    private func fetchBackgroundImageDataIfNeed() {
+        backgroundImage = nil
+        switch emojiArt.background {
+        case .url(let url):
+            // fetch url
+            backgroundImageFetchStatus = .fetching
+            DispatchQueue.global(qos: .userInitiated).async {
+                let imageData = try? Data(contentsOf: url)
+                DispatchQueue.main.async { [weak self] in
+                    // we check after download user change the image?
+                    if self?.emojiArt.background == EmojiArtModel.Background.url(url) {
+                        if let imageData = imageData {
+                            self?.backgroundImage = UIImage(data: imageData)
+                            self?.backgroundImageFetchStatus = .idle
+                        }
+                    }
+                }
+            }
+        case .imageData(let data):
+            backgroundImage = UIImage(data: data)
+        case .blank:
+            break
+        }
+    }
+    
     // MARK: - Intent(s)
     
     func setBackground(_ background: EmojiArtModel.Background) {
         emojiArt.background = background
+        print("set bg \(background)")
     }
     
     func addEmoji(_ emoji: String, at location: (x: Int, y: Int), size: CGFloat)  {
--- LHEmojiArt/EmojiArtDocumentView.swift
+++ LHEmojiArt/EmojiArtDocumentView.swift
@@ -24,18 +24,64 @@ struct EmojiArtDocumentView: View {
         GeometryReader {
             geometry in
             ZStack {
-                Color.yellow
+                Color.white.overlay(
+                    OptionalImage(uiImage: document.backgroundImage)
+                        .position(convertFromEmojiCoordinates((0, 0), in: geometry))
+                )
     //            ForEach(document.emojis) // dont need \.self cause we implement this Indentiable
-                ForEach(document.emojis) {
-                    emoji in
-                    Text(emoji.text)
-                        .font(.system(size: fontSize(for: emoji)))
-                        .position(position(for: emoji, in: geometry))
+                if document.backgroundImageFetchStatus == .fetching {
+                    ProgressView().scaleEffect(2)
+                } else {
+                    ForEach(document.emojis) {
+                        emoji in
+                        Text(emoji.text)
+                            .font(.system(size: fontSize(for: emoji)))
+                            .position(position(for: emoji, in: geometry))
+                    }
+                }
+            }
+            .onDrop(of: [.plainText, .url, .image], isTargeted: nil) {
+                providers, location in
+//                return true
+                return drop(providers: providers, at: location, in: geometry)
+            }
+        }
+    }
+    
+    private func drop(providers: [NSItemProvider], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
+        var found = providers.loadObjects(ofType: URL.self) {
+            url in
+            document.setBackground(EmojiArtModel.Background.url(url.imageURL))
+        }
+        if !found {
+            found = providers.loadObjects(ofType: UIImage.self) {
+                image in
+                if let data = image.jpegData(compressionQuality: 0.1) {
+                    document.setBackground(.imageData(data))
                 }
             }
         }
         
-        
+        if !found {
+            found = providers.loadObjects(ofType: String.self) {
+                string in
+                if let emoji = string.first, emoji.isEmoji {
+                    document.addEmoji(String(emoji),
+                                      at: convertToEmojiCoordinates(location, in: geometry),
+                                      size: defaultEmojiFontSize)
+                }
+            }
+        }
+        return found
+    }
+    
+    private func convertToEmojiCoordinates(_ location: CGPoint, in geometry: GeometryProxy) -> (x: Int, y: Int) {
+        let center = geometry.frame(in: .local).center
+        let location = CGPoint(
+            x: location.x - center.x,
+            y: location.y - center.y
+        )
+        return (Int(location.x), Int(location.y))
     }
     
     private func position(for emoji: EmojiArtModel.Emoji, in geomery: GeometryProxy) -> CGPoint {
@@ -71,6 +117,7 @@ struct ScrollingEmojisView: View {
                 ForEach(emojis.map {String($0)}, id: \.self) {
                     emoji in
                     Text(emoji)
+                        .onDrag{ NSItemProvider(object: emoji as NSString) }
                 }
             }
         }
